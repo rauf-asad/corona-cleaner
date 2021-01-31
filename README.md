c5f0eb71885180427c2a38c3dec342bbfdb45c09
Asad Rauf <asadrauflashari@gmail.com>
2021-01-31 19:38:19 +0500

    Auto update

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # corona-cleaner
+
+This is a repository that combines the data from Johns Hopkins university into one csv file and automatically refrehes the data using github actions. It is what I plan to use for my dashboards while learning tableau. I have something similar going for the current dashboard using power bi. You can check that out on asadrauf.net/corona
---
+++ corona_data_cleaner.py
@@ -0,0 +1,18 @@
+import pandas as pd
+import numpy as np
+import os
+
+
+confirmed = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv')
+deaths = pd.read_csv('https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_deaths_global.csv')
+
+confirmed = pd.melt(confirmed, id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'], var_name='Date', value_name='Confimred')
+deaths = pd.melt(deaths, id_vars=['Province/State', 'Country/Region', 'Lat', 'Long'], var_name='Date', value_name='Deaths')
+
+combined = confirmed.merge(deaths, on=['Province/State', 'Country/Region', 'Lat', 'Long', 'Date'], how='outer')
+
+os.system('git pull')
+combined.to_csv('data.csv', index=False)
+os.system('git add .')
+os.system('git commit -m "Auto update"')
+os.system('git push')
